9ede1b3c3f3aac9674a8551c51f43c19ebd1ea69
am.musin <am.musin@tensor.ru>
2021-05-17 01:35:44 +1100

    fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../../components/Header';
 
 import s from './Home.module.scss';
-import Button from '../../components/Button/index';
+import Button from '../../components/Button';
 import Layout from '../../components/Layout';
 import Parallax from '../../components/Parallax';
 
